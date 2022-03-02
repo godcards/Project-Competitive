4a0f7e63fee0ee697b2d0d5617dbb6f9e3378907
godcards <2929362461@qq.com>
2022-03-02 09:43:23 +0800

    摸鱼

--- src/main/java/com/competitive/service/dayliyservice.java
+++ src/main/java/com/competitive/service/dayliyservice.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class dayliyservice {
+    public class dayliyservice {
 
     @Autowired
     DayliyMapper dayliyMapper;
